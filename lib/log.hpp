50a0324be44db8bdbf44edee466079132179ff1f
Hush-Lee <hush-lee@localhost.localdomain>
2024-11-26 02:35:52 +0800

    init

---
+++ lib/log.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <string>
+
+namespace sylar {
+	class Logger{
+	public:
+		Logger();
+	}
+	class LogAppender{
+	public:
+		virtual ~LogAppender();
+	private:
+
+	}
+	class LogFormatter{
+
+	}
+}
